547a78d55ce5e3798885889669f579c13ae04cd3
Shiyaam Sunder <shiyaamsunder@users.noreply.github.com>
2023-04-25 10:38:35 +0530

    var -> let

--- lab2/computer.js
+++ lab2/computer.js
@@ -1,15 +1,6 @@
-/*Monitors, LCD Screens, LED Screens, Vibrant Colors, 
-• Motherboards, Fast
-• Chips, i9, i7, i5, i3, Core2Duo, Pentium, Very Fast
-• Hard Drives, 2TB, 1TB, 100-500 GB, Fast Reading
-• DVD-ROMs, Burn CDs, Burn DVDs
-• Cases, ATX, AT, Mini, Other Sizes, Choice of Colors. 
-• Power Supplies, we can get one for any computer!
-*/
-
 function display()
 {
-    var arr= [
+    let arr= [
         ["Monitors", "LCD Screens", "LED Screens", "Vibrant Colors"],
         ["Motherboards", "Fast"],
         ["Chips", "i9", "i7", "i5", "i3", "Core2Duo", "Pentium", "Very Fast"],
@@ -19,15 +10,12 @@ function display()
         ["Power Supplies", "we can get one for any computer!"]
     ]
 
-    var s="";
-
-
-
-    for(var i=0;i<arr.length;i++)
+    let s="";
+    for(let i=0;i<arr.length;i++)
     {
-        var s2="";
+        let s2="";
         s2=arr[i][0]+" : ";
-        for(var j=1;j<arr[i].length;j++)
+        for(let j=1;j<arr[i].length;j++)
         {
             s2=s2+ arr[i][j]+" ,";
         }
