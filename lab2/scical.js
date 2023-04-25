751929d0e995768875f3e6cbc9fa1740513e184f
shiyaam <63956643+shiyaamsunder@users.noreply.github.com>
2023-04-25 10:53:34 +0530

    calc

--- lab2/scical.js
+++ lab2/scical.js
@@ -45,10 +45,31 @@ function addNumber(e) {
 function addOperator(e) {
   const op = e.target.textContent;
   if(op === "sin"){
-
-    console.log(Math.sin(Number(input.value * Math.PI/180)))
-
+    input.value  = Math.sin(Number(input.value * Math.PI/180))
   }
+  else if(op === "cos"){
+    input.value  = Math.cos(Number(input.value * Math.PI/180))
+  }
+  else if(op === "tan"){
+    input.value  = Math.tan(Number(input.value * Math.PI/180))
+  }
+  else if(op === "abs"){
+    input.value  = Math.abs(Number(input.value))
+  }
+  else if(op === "log"){
+    input.value  = Math.log(Number(input.value))
+  }
+  else if(op === "log10"){
+    input.value  = Math.log10(Number(input.value))
+  }
+  else if(op === "n!"){
+    input.value  = factorial(Number(input.value))
+  }
+
+  else if(op === "pow"){
+    input.value+= "**"
+  }
+
   else input.value+=op;
   
 
@@ -63,3 +84,18 @@ function calculateResult() {
 
 }
 
+function factorial(n){
+  let answer = 1;
+  if (n == 0 || n == 1){
+    return answer;
+  }
+  else if(n > 1){
+    for(var i = n; i >= 1; i--){
+      answer = answer * i;
+    }
+    return answer;
+  }
+  else{
+    return "number has to be positive."
+  }  
+}
