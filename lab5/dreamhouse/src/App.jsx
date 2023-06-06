c1f986334feef385f4b60572585e5bff2ecfea81
shiyaam <shiyaamsunderms@gmail.com>
2023-06-06 16:17:32 +0530

    lab2 questions 1 and 2

---
+++ lab5/dreamhouse/src/App.jsx
@@ -0,0 +1,105 @@
+import { Component } from 'react'
+import './App.css'
+
+
+class App extends Component {
+
+
+  state = {
+    months: 0,
+    annual_salary: "",
+    total_cost: "",
+    portion_saved: "",
+    down_payment: "",
+  }
+
+  handleChange = (e) => {
+
+    this.setState({ ...this.state, [e.target.name]: e.target.value })
+
+    // if (this.state[e.target.name].toString().length > 1) {
+    //   this.setState({ ...this.state, disabled: false })
+    // }
+    // else {
+    //   this.setState({ ...this.state, disabled: true })
+    // }
+
+    // if (this.state.annual_salary.toString().length > 1 &&
+    //   this.state.total_cost.toString().length > 1 &&
+    //   this.state.portion_saved.toString().length > 1) {
+    //
+    //   this.setState({ ...this.state, disabled: false })
+    // }
+    // else {
+    //   this.setState({ ...this.state, disabled: true })
+    // }
+  }
+
+  handleSubmit = () => {
+    const { annual_salary, portion_saved, total_cost } = this.state;
+    console.log(annual_salary);
+
+    if (portion_saved > 100) {
+      alert("should be <= 100")
+      return;
+    }
+
+    if (annual_salary.toString().length < 1 ||
+      total_cost.toString().length < 1 ||
+      portion_saved.toString().length < 1) {
+
+      alert("Enter all the inputs");
+      return;
+
+    }
+
+
+    const down_payment = 0.25 * total_cost;
+    const savings_per_year = portion_saved * annual_salary;
+    const savings_per_month = savings_per_year / 12;
+    const total_months_needed = down_payment / savings_per_month
+
+    this.setState({ ...this.state, down_payment: down_payment, months: total_months_needed })
+
+
+
+  }
+  render() {
+
+
+    return (
+      <>
+        <main>
+          <h1> Downpayment Calculator</h1>
+
+
+          <div className="inputs">
+            <div className='input__wrapper'>
+              <label className='input__label'>Annual Salary </label>
+              <input className='input' value={this.state.annual_salary} name="annual_salary" onChange={this.handleChange} type="number" />
+            </div>
+            <div className='input__wrapper'>
+              <label className='input__label'>Portion Saved</label>
+              <input className='input' value={this.state.portion_saved} max={100} placeholder='Should be in percentage' name="portion_saved" onChange={this.handleChange} type="number" />
+            </div>
+            <div className='input__wrapper'>
+              <label className='input__label'>Total cost of the House</label>
+              <input className='input' value={this.state.total_cost} name="total_cost" onChange={this.handleChange} type="number" />
+            </div>
+
+
+          </div>
+
+          <button onClick={this.handleSubmit} >Calculate</button>
+
+          <div className='months'>
+            <p>Down Payment : {this.state.down_payment}</p>
+            Total Months: {Math.round(this.state.months)}
+          </div>
+
+        </main>
+      </>
+    )
+  }
+}
+export default App
---
+++ lab5/electricity/src/App.jsx
@@ -0,0 +1,84 @@
+import './App.css'
+import { useState } from 'react';
+
+
+function Item({ message, value = 0.0 }) {
+  return (
+
+    <div className="item">
+      <div className="item__left">
+        {message}
+      </div>
+      <div className='item__right'>
+        {value}
+      </div>
+    </div>
+
+  )
+}
+
+
+
+
+function App() {
+
+  const [units, setUnits] = useState(0.0);
+  const [netAmount, setNetAmount] = useState(0);
+  const [currentCharge, setCurrentCharge] = useState(0);
+
+
+  const handleSubmit = () => {
+
+    if (units == 0) {
+      alert("Enter valid units");
+    }
+    setNetAmount(calculateBill(units));
+  }
+  const calculateBill = () => {
+    let subsidy = 250;
+    let f = 0;
+    if (units >= 0 && units <= 100) f = 0;
+    else if (units > 100 && units <= 150) f = 100;
+    else if (units > 150 && units <= 200) f = 100 + 3.75 * units;
+    else if (units > 200 && units <= 350) f = 250;
+    else if (units > 350 && units <= 400) f = 250 + 4 * units;
+    else if (units > 400 && units <= 450) f = 300;
+    else if (units > 450 && units <= 600) f = 300 + 4 * units;
+    else f = 400 + 5 * (units - 600);
+
+    console.log(f);
+
+    if (f < 250) subsidy = 0;
+
+    setCurrentCharge(f);
+    return f + 50 - subsidy;
+  }
+  return (
+    <>
+      <main className="main__wrapper">
+
+        <h1>Electricity Bill</h1>
+
+        <div>
+          <label>Enter the units: </label>
+          <input type="number" onChange={(e) => setUnits(Number(e.target.value))} />
+        </div>
+        <button style={{ marginTop: "10px", }} onClick={handleSubmit}>Calculate</button>
+
+
+        <div className="wrapper">
+          <Item message="Consumed Units" value={units} />
+          <Item message="Total Current Charge Rs:" value={currentCharge} />
+          <Item message="Current Charge Rs." value={currentCharge} />
+          <Item message="Fixed Cost Rs.(+)" value={50} />
+          <Item message="Subsidy Fixed Cost Rs.(-)" />
+          <Item message="New Subsidy Rs.(-)" value={250} />
+          <Item message="Net Amount" value={netAmount} />
+
+        </div>
+      </main >
+    </>
+  )
+}
+
+export default App
