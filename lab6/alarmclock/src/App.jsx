5384e70e378ac7209ed8731c8c5ff40b4588e3b1
shiyaam <shiyaamsunderms@gmail.com>
2023-06-20 14:27:47 +0530

    added up down animations

--- lab6/alarmclock/src/App.jsx
+++ lab6/alarmclock/src/App.jsx
@@ -1,23 +1,72 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(
-    `${new Date().getHours()}:${new Date().getMinutes()}`
-  )
+  const [currentHours, setCurrentHours] = useState(0)
+  const [currentMinutes, setCurrentMinutes] = useState(0)
+  // const [selectedDays, setSelectedDays] = useState('')
+  const currentHourRef = useRef(null)
+  const timerRef = useRef(null)
 
   useEffect(() => {
+    const formatTime = (timeUnit) => {
+      if (timeUnit.toString().length == 1) {
+        return `0${timeUnit}`
+      }
+
+      return timeUnit
+    }
+    const update = () => {
+      const date = new Date()
+      setCurrentHours(formatTime(date.getHours()))
+      setCurrentMinutes(formatTime(date.getMinutes()))
+    }
+    update()
     const interval = setInterval(() => {
-      setCurrentTime(`${new Date().getHours()}:${new Date().getMinutes()}`)
-    }, 1000 * 60)
+      update()
+    }, 1000)
 
     return () => clearInterval(interval)
   }, [])
+
+  useEffect(() => {
+    return () => clearTimeout(timerRef.current)
+  }, [])
+
+  const handleClick = (className) => {
+    let hours = currentHourRef.current
+    if (hours) {
+      hours.classList.toggle(className)
+      timerRef.current = setTimeout(
+        () => hours.classList.remove(className),
+        1000
+      )
+    }
+  }
+
   return (
     <>
       <main>
         <div className='currentTime'>
-          <span>{currentTime}</span>
+          <span>
+            {currentHours}:{currentMinutes}
+          </span>
+        </div>
+
+        <div className='selectTime'>
+          <div className='selectHours'>
+            <button className='change' onClick={() => handleClick('moveup')}>
+              down
+            </button>
+            <div className='hours__wrapper'>
+              <div ref={currentHourRef} className='hours'>
+                {currentHours}
+              </div>
+            </div>
+            <button className='change' onClick={() => handleClick('movedown')}>
+              up
+            </button>
+          </div>
         </div>
       </main>
     </>
