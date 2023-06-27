33fea85ab03944bc85934f88ab14b784f5225d9a
shiyaam <shiyaamsunderms@gmail.com>
2023-06-27 20:31:04 +0530

    finished alarm

--- lab6/alarmclock/src/App.jsx
+++ lab6/alarmclock/src/App.jsx
@@ -2,71 +2,90 @@ import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [currentHours, setCurrentHours] = useState(0)
-  const [currentMinutes, setCurrentMinutes] = useState(0)
-  // const [selectedDays, setSelectedDays] = useState('')
-  const currentHourRef = useRef(null)
-  const timerRef = useRef(null)
+  const [displayTime, setDisplayTime] = useState('')
+  const [alarms, setAlarms] = useState([])
+
+  const alarmRef = useRef(null)
+  const noteRef = useRef(null)
 
   useEffect(() => {
-    const formatTime = (timeUnit) => {
-      if (timeUnit.toString().length == 1) {
-        return `0${timeUnit}`
-      }
-
-      return timeUnit
-    }
     const update = () => {
       const date = new Date()
-      setCurrentHours(formatTime(date.getHours()))
-      setCurrentMinutes(formatTime(date.getMinutes()))
+      setDisplayTime(
+        date.toLocaleString('en-GB', {
+          hour: 'numeric',
+          minute: 'numeric',
+          hour12: 'true',
+        })
+      )
+
+      if (alarms.length > 0 && date.getTime() >= alarms[0].time) {
+        alert('alarm')
+        let copy = alarms
+        copy.shift()
+        setAlarms(copy)
+      }
     }
+
     update()
     const interval = setInterval(() => {
       update()
     }, 1000)
 
     return () => clearInterval(interval)
-  }, [])
+  }, [alarms])
 
-  useEffect(() => {
-    return () => clearTimeout(timerRef.current)
-  }, [])
-
-  const handleClick = (className) => {
-    let hours = currentHourRef.current
-    if (hours) {
-      hours.classList.toggle(className)
-      timerRef.current = setTimeout(
-        () => hours.classList.remove(className),
-        1000
-      )
+  const handleClick = () => {
+    if (!alarmRef.current.value || !noteRef.current.value) return
+    let alarmDate = new Date(alarmRef.current.value)
+    if (alarms.find((a) => a == alarmDate.getTime())) {
+      return
     }
+
+    let copy = [...alarms]
+
+    copy.push({ note: noteRef.current.value, time: alarmDate.getTime() })
+
+    let sorted = copy.sort((a, b) => a.time - b.time)
+    setAlarms(sorted)
+    alarmRef.current.value = ''
+    noteRef.current.value = ''
   }
 
+  const handleRemove = (time) => {
+    let removedArray = [...alarms].filter((a) => a.time !== time)
+    setAlarms(removedArray)
+  }
   return (
     <>
       <main>
         <div className='currentTime'>
-          <span>
-            {currentHours}:{currentMinutes}
-          </span>
+          <span>{displayTime.toUpperCase()}</span>
         </div>
-
-        <div className='selectTime'>
-          <div className='selectHours'>
-            <button className='change' onClick={() => handleClick('moveup')}>
-              down
-            </button>
-            <div className='hours__wrapper'>
-              <div ref={currentHourRef} className='hours'>
-                {currentHours}
+        <input type='text' ref={noteRef} />
+        <input
+          type='datetime-local'
+          min={new Date().toISOString().slice(0, 16)}
+          ref={alarmRef}
+        />
+        <button onClick={handleClick}>Add an alarm</button>
+        <p>Upcoming Alarms</p>
+        <div className='alarm__list'>
+          {alarms.map((alarm, i) => (
+            <div className='alarm__item' key={i}>
+              <div className='left'>
+                <div className='alarm__note'>{alarm.note}</div>
+                <div className='alarm__time'>
+                  {new Date(alarm.time).toLocaleString('en-GB', {
+                    hour12: true,
+                  })}
+                </div>
+              </div>
+              <div className='right'>
+                <button onClick={() => handleRemove(alarm.time)}>Remove</button>
               </div>
             </div>
-            <button className='change' onClick={() => handleClick('movedown')}>
-              up
-            </button>
-          </div>
+          ))}
         </div>
       </main>
     </>
